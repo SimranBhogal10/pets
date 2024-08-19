cfa2859cb2a81586e58d4136ecfd4830d115c942
SimranBhogal10 <simran.trilochan@anywhere.co>
2024-08-19 13:13:49 +0530

    chore: added env variable

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,15 @@ import HomeBanner from "@/app/components/HomeBanner";
 
 import ProductList from "./listing/ProductList";
 import { Suspense } from "react"; 
+import { NextResponse } from "next/server";
 export default async function Home() {
 
-  const productData = await fetch(`https://storage.googleapis.com/testing-sb6.appspot.com/SB6/Users/simran/products.json`);
+  const CLOUD_STORAGE_URL = process.env.NEXT_PUBLIC_CLOUD_STORAGE_URL;
+
+  if(!CLOUD_STORAGE_URL){
+    return NextResponse.error();
+  }
+  const productData = await fetch(CLOUD_STORAGE_URL);
   const data = await productData.json();
   return (
       <div className="p-12">
