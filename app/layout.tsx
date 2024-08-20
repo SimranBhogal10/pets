b221effe6a64b55fc547b5e6db0b089a94a51927
SimranBhogal10 <simran.trilochan@anywhere.co>
2024-08-20 19:09:35 +0530

    chore: added the meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,25 +25,24 @@ export default function RootLayout({
   return (
     <html lang="en">
        <Head>
-        <title>VR Pets - Pet Store</title>
-        <meta name="description" content={`Buy products for your fury friends. Best quality at the best price.`} />
 
-        <meta property="og:title" content={`VR Pets`} />
-        <meta property="og:description" content={`Buy products for your fury friends. Best quality at the best price.`} />
-        <meta property="og:image" content='/assets/clogo.png' />
-        <meta property="og:image" content="https://ogcdn.net/e4b8c678-7bd5-445d-ba03-bfaad510c686/v4/pets-umber.vercel.app/E-Pets/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fimages%2F32b39a13-877d-47d0-801d-aae3c088620a.png%3Ftoken%3DQ4tBb7g5POKOJ4dM_3o_UonrpfUchAWxUDeybp8NYeU%26height%3D872%26width%3D1080%26expires%3D33260160006/og.png" />
-        <meta property="og:type" content="product" />
+        <title>E-Pets</title>
+        <meta name="description" content="Your One-Stop Pet Shop" />
+
 
         <meta property="og:url" content="https://pets-umber.vercel.app/" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="VR Pets" />
-        <meta property="og:description" content="Your One-Stop Pet Shop"/ >
-        <meta property="og:image" content="https://ogcdn.net/e4b8c678-7bd5-445d-ba03-bfaad510c686/v4/pets-umber.vercel.app/E-Pets/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fimages%2F32b39a13-877d-47d0-801d-aae3c088620a.png%3Ftoken%3DQ4tBb7g5POKOJ4dM_3o_UonrpfUchAWxUDeybp8NYeU%26height%3D872%26width%3D1080%26expires%3D33260160006/og.png"/>
+        <meta property="og:title" content="E-Pets" />
+        <meta property="og:description" content="Your One-Stop Pet Shop" />
+        <meta property="og:image" content="https://ogcdn.net/e4b8c678-7bd5-445d-ba03-bfaad510c686/v4/pets-umber.vercel.app/E-Pets/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fimages%2F32b39a13-877d-47d0-801d-aae3c088620a.png%3Ftoken%3DQ4tBb7g5POKOJ4dM_3o_UonrpfUchAWxUDeybp8NYeU%26height%3D872%26width%3D1080%26expires%3D33260160006/og.png" />
 
-        <meta name="twitter:title" content={`VR Pets`} />
-        <meta name="twitter:description" content={`Buy products for your fury friends. Best quality at the best price.`} />
-        <meta property="og:image" content="https://ogcdn.net/e4b8c678-7bd5-445d-ba03-bfaad510c686/v4/pets-umber.vercel.app/E-Pets/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fimages%2F32b39a13-877d-47d0-801d-aae3c088620a.png%3Ftoken%3DQ4tBb7g5POKOJ4dM_3o_UonrpfUchAWxUDeybp8NYeU%26height%3D872%26width%3D1080%26expires%3D33260160006/og.png"/>
+
         <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="pets-umber.vercel.app" />
+        <meta property="twitter:url" content="https://pets-umber.vercel.app/" />
+        <meta name="twitter:title" content="E-Pets" />
+        <meta name="twitter:description" content="Your One-Stop Pet Shop" />
+        <meta name="twitter:image" content="https://ogcdn.net/e4b8c678-7bd5-445d-ba03-bfaad510c686/v4/pets-umber.vercel.app/E-Pets/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fimages%2F32b39a13-877d-47d0-801d-aae3c088620a.png%3Ftoken%3DQ4tBb7g5POKOJ4dM_3o_UonrpfUchAWxUDeybp8NYeU%26height%3D872%26width%3D1080%26expires%3D33260160006/og.png" />
       </Head>
       <body className={maven.className}>
         <Toaster toastOptions={{
