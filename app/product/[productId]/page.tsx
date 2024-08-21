2a9ccbb782518cbd2f3ca67c39648db7d6aeb587
SimranBhogal10 <simran.trilochan@anywhere.co>
2024-08-21 07:07:13 +0530

    chore: added meta tags to the product deteails page

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -9,6 +9,30 @@ type ProductParams = {
     productId:string
 }
 
+export async function generateMetadata({ params }: {params: ProductParams}) {
+    const product = petProducts.find((item) => item.id == params.productId);
+
+    if(product)
+    return {
+      title: `${product.name} - VR Pets`,
+      description: `Buy ${product.name} at VR Pets . Best quality at the best price.`,
+      openGraph: {
+        title: `${product.name} - VR Pets`,
+        description: `Buy ${product.name} at VR Pets. Best quality at the best price.`,
+        url: `https://pets-umber.vercel.app/product/${params.productId}`,
+        images: [
+          {
+            url: product.image,
+            width: 800,
+            height: 600,
+            alt: product.name,
+          },
+        ],
+        type: 'product',
+      },
+    };
+  }
+
 const Product = ({params} : {params: ProductParams}) =>{
     console.log(params);
 
